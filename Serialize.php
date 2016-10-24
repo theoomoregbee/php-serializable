ffb729bd148e9e6ae4f78a6414c6ef176de3b0b6
Theophy Omoregbee <theo4u@ymail.com>
2016-10-25 00:39:03 +0100

    example and Serializable added

---
+++ Serialize.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Theophilus Omoregbee <theo4u@ymail.com>
+ * Date: 10/25/16
+ * Time: 12:11 AM
+ 
+* This class serialize php object class to an instance compatible with json ecode
+*/
+class SerializeMe
+{
+
+	 /**
+     * This serialize our object to a normal single object
+     * @param $object
+     * @return array
+     */
+    public static function serialize($object){
+        $reflect = new ReflectionClass($object);
+        $props   = $reflect->getDefaultProperties();
+
+        foreach($props as $key => $value){
+           $check_references = explode("_", $key);
+            $getter = "";
+            if(count($check_references) > 0){
+                foreach($check_references as $reference)
+                   $getter .= ucfirst($reference);
+            }else
+             $getter = ucfirst($key);
+
+            $getter = "get".$getter;
+            if(is_object($object->$getter()))
+              $props[$key] = SerializeMe::serialize($object->$getter());//continue serialization, till all object is free from serialization
+            else
+                if(is_array($object->$getter())) {
+                $props[$key] = array();
+                    foreach ($object->$getter() as $anObject)
+                        if(is_object($anObject))
+                         $props[$key][] = SerializeMe::serialize($anObject);//continue serialization till we return normal value
+                        else
+                            $props[$key][] = $anObject;
+                }else
+              $props[$key] = $object->$getter();
+        }
+
+        return $props;
+    }
+}
+?>
+
+
---
+++ example.php
@@ -0,0 +1,270 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Theophilus Omoregbee <theo4u@ymail.com>
+ * Date: 10/25/16
+ * Time: 12:10 AM
+ */
+
+include "Serialize.php";
+
+/**
+* this handles the parents of the user
+*/
+class ParentTest
+{
+	protected $email;
+	protected $password;
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = md5($password);
+    }
+}
+
+class State{
+    private $name;
+    private $code;
+
+    function __construct($name, $code)
+    {
+        $this->name = $name;
+        $this->code = $code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+}
+
+class Country{
+    private $name;
+    private $code;
+
+    function __construct($name, $code)
+    {
+        $this->name = $name;
+        $this->code = $code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+
+}
+
+/**
+* Create a dummy user
+*/
+class User extends ParentTest
+{
+    private $firstname;
+    private $lastname;
+    private $states=array();
+    private $country;
+    private $dateCreated;
+    private $roles = array();
+
+	
+	function __construct()
+	{
+        $this->dateCreated = time();
+	}
+
+    /**
+     * @return mixed
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param mixed $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param mixed $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+    /**
+     * @return mixed
+     */
+    public function getStates()
+    {
+        return $this->states;
+    }/**
+     * @param mixed $states
+     */
+    public function setStates($states)
+    {
+        $this->states = $states;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param int $dateCreated
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param array $roles
+     */
+    public function setRoles($roles)
+    {
+        $this->roles = $roles;
+    }
+
+}
+
+//done creating dummy objects
+$user = new User();
+$user->setFirstname("Theophilus");
+$user->setLastname("Omoregbee");
+
+$user->setStates(array(new State("Edo state", "ED"), new State("Lagos State", "LG")));
+$user->setCountry(new Country("Nigeria","NG"));
+
+$user->setRoles(array("ADMIN","DB MANAGER"));
+
+$user->setEmail("theo4u@ymail.com");
+$user->setPassword("1111");
+
+//lets serialize our object now
+echo json_encode(SerializeMe::serialize($user));
+
+?>
